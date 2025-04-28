6dd20dce251de7638a1a5e5aa5ea673054b17e56
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-04-28 21:51:56 +0300

    Add function get_doctors()
    get_doctors() returns dict with info about doctors

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-import csv
-import src.doctor_data as doctor_data
-
-
-def parse_schedule_csv_file(csv_file: str):
-    parsed_csv = {}
-
-    with open(csv_file, 'r', newline='') as csv_file:
-        reader = list(csv.reader(csv_file))[2:]
-        for row in reader:
-            print(doctor_data.get_doctor_data(row))
+import src.doctors as doctors
+import src. 
 
 
 def main():
-    filename = '/home/user/Downloads/archimed_doctors.sql.csv'
-    parse_schedule_csv_file(filename)
+    print(doctor.parse_doctors('archimed_doctors.sql.csv'))
 
 
 if __name__ == '__main__':
---
+++ src/doctors.py
@@ -0,0 +1,28 @@
+import csv
+
+
+def calculate_timescale(*, timescale: str, max_time: str, default: int=30) -> int:
+    timescale = int(timescale) if timescale != '' else 0
+    if timescale > 0:
+        return timescale
+
+    max_time = int(max_time) if max_time != '' else 0
+    if max_time > 0:
+        return max_time
+
+    return default
+
+
+def get_doctor(row: list[str]) -> dict:
+    return { "fullname": row[5], "espec": '', 'timescale': calculate_timescale(timescale=row[7], max_time=row[8]) }
+
+
+def parse_doctors(csv_file: str) -> dict:
+    doctors = {}
+
+    with open(csv_file, 'r', newline='') as csv_file:
+        reader = list(csv.reader(csv_file))[2:]
+        for row in reader:
+            doctors[ row[0] ] = get_doctor(row)
+
+    return doctors
