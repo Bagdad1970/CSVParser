c3869bc8f43ce97abf3793248cc1cc7cce78c866
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-04-29 00:52:35 +0300

    Create parsing for doctors

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import src.doctors as doctors
-import src. 
+import src.schedules as schedules
 
 
 def main():
-    print(doctor.parse_doctors('archimed_doctors.sql.csv'))
+    _doctors = doctors.parse_doctors('archimed_doctors.sql.csv')
+
+    doctor_timescales = doctors.match_doctors_and_timescales(_doctors)
+
+    _schedules = schedules.parse_schedules('archimed_doctorsshedule.sql.csv', doctor_timescales)
+    print(_schedules)
 
 
 if __name__ == '__main__':
--- src/doctors.py
+++ src/doctors.py
@@ -16,12 +16,15 @@ def calculate_timescale(*, timescale: str, max_time: str, default: int=30) -> in
 def get_doctor(row: list[str]) -> dict:
     return { "fullname": row[5], "espec": '', 'timescale': calculate_timescale(timescale=row[7], max_time=row[8]) }
 
+def match_doctors_and_timescales(doctors: dict) -> dict:
+    return { doctor_id : info.get('timescale') for doctor_id, info in doctors.items() }
+
 
 def parse_doctors(csv_file: str) -> dict:
     doctors = {}
 
     with open(csv_file, 'r', newline='') as csv_file:
-        reader = list(csv.reader(csv_file))[2:]
+        reader = list(csv.reader(csv_file))[2 : ]
         for row in reader:
             doctors[ row[0] ] = get_doctor(row)
 
